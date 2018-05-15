59575d4f7784bcf43400d37e1c8696693d581fa7
James Thompson <cthompson88@ymail.com>
2018-05-15 16:11:04 +1200

    Grayson needs help

--- HRWells/App_Start/RouteConfig.cs
+++ HRWells/App_Start/RouteConfig.cs
@@ -14,6 +14,11 @@ namespace HRWells
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "ListHorsesByAttribute",
+                url: "horse/list/{attrib}/{value}",
+                defaults: new { controller = "Horse", action = "HorsesByAttrib", value=UrlParameter.Optional}
+                );
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
--- HRWells/Controllers/HorseController.cs
+++ HRWells/Controllers/HorseController.cs
@@ -20,5 +20,59 @@ namespace HRWells.Controllers
                 return View(horses);
             }
         }
+        public ActionResult HorsesByAttrib(string attrib, string value)
+        {
+            IEnumerable<Horse> theList;
+            float floatVal;
+            int intVal;
+
+            using (ApplicationDbContext context = new ApplicationDbContext())
+            {
+
+                switch (attrib)
+                {
+                    case "weight":
+                        floatVal = float.Parse(value);
+                        theList = context.Horses
+                           .Where(h => h.Weight == floatVal)
+                           .Include(h => h.Breed)
+                           .ToList();
+                        break;
+
+                    case "age":
+                        intVal = int.Parse(value);
+                        theList = context.Horses
+                           .Where(h => h.Age == intVal)
+                           .Include(h => h.Breed)
+                           .ToList();
+
+                        break;
+
+                    case "number":
+                        intVal = int.Parse(value);
+                        theList = context.Horses
+                           .Where(h => h.Number == intVal)
+                           .Include(h => h.Breed)
+                           .ToList();
+
+                        break;
+
+                    case "owner":
+                        theList = context.Horses
+                           .Where(h => h.Owner == (string)value)
+                           .Include(h => h.Breed)
+                           .ToList();
+
+                        break;
+
+
+                    default:
+                        return Content("Not found");
+                }
+
+                return View("Index", theList);
+
+            }
+        }
     }
 }
