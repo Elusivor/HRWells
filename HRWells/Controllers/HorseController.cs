3b6e9d30c01c0be3ac675564e9c63f705e584799
James Thompson <cthompson88@ymail.com>
2018-05-21 13:37:47 +1200

    update for henry

--- HRWells/Controllers/HorseController.cs
+++ HRWells/Controllers/HorseController.cs
@@ -10,15 +10,16 @@ namespace HRWells.Controllers
 {
     public class HorseController : Controller
     {
+        ApplicationDbContext _context;
+        public HorseController()
+        {
+            _context = new ApplicationDbContext();
+        }
         // GET: Horse
         public ActionResult Index()
         {
-
-            using (ApplicationDbContext context = new ApplicationDbContext())
-            {
-                var horses = context.Horses.Include(h => h.Breed).ToList();
-                return View(horses);
-            }
+            var horses = _context.Horses.Include(h => h.Breed).ToList();
+            return View(horses);
         }
         public ActionResult HorsesByAttrib(string attrib, string value)
         {
@@ -26,58 +27,67 @@ namespace HRWells.Controllers
             float floatVal;
             int intVal;
 
-            using (ApplicationDbContext context = new ApplicationDbContext())
+
+            switch (attrib)
             {
+                case "weight":
+                    floatVal = float.Parse(value);
+                    theList = _context.Horses
+                       .Where(h => h.Weight == floatVal)
+                       .Include(h => h.Breed)
+                       .ToList();
+                    break;
 
-                switch (attrib)
-                {
-                    case "weight":
-                        floatVal = float.Parse(value);
-                        theList = context.Horses
-                           .Where(h => h.Weight == floatVal)
-                           .Include(h => h.Breed)
-                           .ToList();
-                        break;
+                case "age":
+                    intVal = int.Parse(value);
+                    theList = _context.Horses
+                       .Where(h => h.Age == intVal)
+                       .Include(h => h.Breed)
+                       .ToList();
 
-                    case "age":
-                        intVal = int.Parse(value);
-                        theList = context.Horses
-                           .Where(h => h.Age == intVal)
-                           .Include(h => h.Breed)
-                           .ToList();
+                    break;
 
-                        break;
+                case "number":
+                    intVal = int.Parse(value);
+                    theList = _context.Horses
+                       .Where(h => h.Number == intVal)
+                       .Include(h => h.Breed)
+                       .ToList();
 
-                    case "number":
-                        intVal = int.Parse(value);
-                        theList = context.Horses
-                           .Where(h => h.Number == intVal)
-                           .Include(h => h.Breed)
-                           .ToList();
+                    break;
 
-                        break;
+                case "owner":
+                    theList = _context.Horses
+                       .Where(h => h.Owner == (string)value)
+                       .Include(h => h.Breed)
+                       .ToList();
 
-                    case "owner":
-                        theList = context.Horses
-                           .Where(h => h.Owner == (string)value)
-                           .Include(h => h.Breed)
-                           .ToList();
+                    break;
+                case "breed":
+                    theList = _context.Horses
+                        .Include(h => h.Breed)
+                        .Where(h => h.Breed.Name == (string)value)
+                        .ToList();
+                    break;
 
-                        break;
-                    case "breed":
-                        theList = context.Horses
-                            .Include(h => h.Breed)
-                            .Where(h => h.Breed.Name == (string)value)
-                            .ToList();
-                        break;
-
-                    default:
-                        return Content("Not found");
-                }
-
-                return View("Index", theList);
-
+                default:
+                    return Content("Not found");
             }
+
+            return View("Index", theList);
+
+        }
+
+        public ActionResult Single(int ID)
+        {
+            //Fetch the horse record from the database
+            Horse MyHorse;
+
+            MyHorse = _context.Horses.Find(ID);
+            if (MyHorse != null)
+                return HttpNotFound();
+            return View(MyHorse);
+
         }
     }
 }
