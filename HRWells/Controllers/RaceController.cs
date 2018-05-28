787218d52c72d8146648a7a62c43672e1f33c850
James Thompson <cthompson88@ymail.com>
2018-05-28 19:25:17 +1200

    Fixed delete Dialog backdrop issue

---
+++ HRWells/Controllers/RaceController.cs
@@ -0,0 +1,50 @@
+﻿using HRWells.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace HRWells.Controllers
+{
+    public class RaceController : Controller
+    {
+        private ApplicationDbContext _context;
+        public RaceController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Race
+        public ActionResult Index()
+        {
+
+            var Race = _context.Races.Include(r => r.Track);
+            return View(Race);
+        }
+        public ActionResult Single(int ID)
+        {
+            Race MyRace;
+            MyRace = _context.Races.Include(t => t.Track).SingleOrDefault(r => r.ID == ID);
+            if (MyRace == null)
+            {
+                return HttpNotFound();
+            }
+            return View(MyRace);
+        }
+        public ActionResult Delete(int ID)
+        {
+            var race = _context.Races.Find(ID);
+            if (race == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                _context.Races.Remove(_context.Races.Find(ID));
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
